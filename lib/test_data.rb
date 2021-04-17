dce3fa1d97cfd820c4ddba80e5da659a00ae7f2f
Justin Searls <searls@gmail.com>
2021-04-17 12:43:42 -0400

    bundle gem test_data

---
+++ lib/test_data.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "test_data/version"
+
+module TestData
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "test_data"
+
+require "minitest/autorun"
