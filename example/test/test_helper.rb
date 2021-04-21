ab1a9ec4c024ace44fd65cc56d5bae60a8284fdb
Justin Searls <searls@gmail.com>
2021-04-21 09:42:20 -0400

    prevent commits to the test db

--- example/test/test_helper.rb
+++ example/test/test_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+Noncommittal.start!
+
 class SerializedNonTransactionalTestCase < ActiveSupport::TestCase
   parallelize(workers: 1)
   self.use_transactional_tests = false
