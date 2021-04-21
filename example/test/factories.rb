cc2e32a505b20de999239d8b999aaff0744b7784
Justin Searls <searls@gmail.com>
2021-04-20 20:36:49 -0400

    add a new test to demo switching between test data

---
+++ example/test/factories.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :boop do
+  end
+end
---
+++ example/test/integration/mode_switching_demo_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+class ModeSwitchingTestCase < ActiveSupport::TestCase
+  fixtures []
+
+  def self.test_data_mode(mode)
+    if mode == :factory_bot
+      require "factory_bot_rails"
+      include FactoryBot::Syntax::Methods
+
+      setup do
+        TestData.reset
+        ActiveRecord::Base.connection.begin_transaction(joinable: false, _lazy: false)
+      end
+
+      teardown do
+        ActiveRecord::Base.connection.rollback_transaction
+      end
+
+    elsif mode == :test_data
+      self.use_transactional_tests = false
+
+      setup do
+        TestData.load_data_dump
+      end
+
+      teardown do
+        TestData.rollback
+      end
+    end
+  end
+end
+
+class FactoryModeTest < ModeSwitchingTestCase
+  test_data_mode :factory_bot
+
+  def test_boops
+    create(:boop)
+
+    assert_equal 1, Boop.count
+  end
+end
+
+class TestDataModeTest < ModeSwitchingTestCase
+  test_data_mode :test_data
+
+  def test_boops
+    assert_equal 10, Boop.count
+  end
+end
