2c02e7cb4c3b5eb9c63fbdd109174ac639155231
Justin Searls <searls@gmail.com>
2021-06-22 16:06:14 -0400

    Print warnings about file size when dumping database files

--- lib/test_data/dumps_database.rb
+++ lib/test_data/dumps_database.rb
@@ -40,10 +40,12 @@ module TestData
     def dump(type:, database_name:, relative_path:, full_path:, name: type, flags: "")
       dump_pathname = Pathname.new(full_path)
       FileUtils.mkdir_p(File.dirname(dump_pathname))
+      before_size = File.size?(dump_pathname)
       if execute("pg_dump #{database_name} --no-tablespaces --no-owner --inserts --#{type}-only #{flags} -f #{dump_pathname}")
         prepend_set_replication_role!(full_path) if type == :data
 
         TestData.log.info "Dumped '#{database_name}' #{name} to '#{relative_path}'"
+        log_size_info_and_warnings(before_size: before_size, after_size: File.size(dump_pathname))
       else
         raise "Failed while attempting to  dump '#{database_name}' #{name} to '#{relative_path}'"
       end
@@ -71,6 +73,27 @@ module TestData
         EOF
       COMMAND
       TestData.log.debug("Prepended replication role instruction to '#{data_dump_path}'")
+    end
+
+    def log_size_info_and_warnings(before_size:, after_size:)
+      percent_change = percent_change(before_size, after_size)
+      TestData.log.info "  Size: #{to_size(after_size)}#{" (#{percent_change}% #{before_size > after_size ? "decrease" : "increase"})" if percent_change}"
+      if after_size > 5242880
+        TestData.log.warn "  WARNING: file size exceeds 5MB. Be sure to only persist what data you need to sufficiently test your application"
+      end
+      if before_size && (after_size - before_size) > 1048576
+        TestData.log.warn "  WARNING: size of this dump increased by #{to_size(after_size - before_size)}. You may want to inspect the file to validate extraneous data was not committed"
+      end
+    end
+
+    def percent_change(before_size, after_size)
+      return unless before_size && before_size > 0 && after_size
+      ((before_size - after_size).abs / before_size * 100).round(2)
+    end
+
+    def to_size(bytes)
+      e = Math.log10(bytes).to_i / 3
+      "%.0f" % (bytes / 1000**e) + [" bytes", "KB", "MB", "GB"][e]
     end
   end
 end
