a7f5106de8b0651402821322865738e2a2253897
Justin Searls <searls@gmail.com>
2021-06-22 16:06:01 -0400

    Ensure that the rails server prints to stdout in test_data mode
    (Rails hard-codes this to development)

--- lib/generators/test_data/environment_file_generator.rb
+++ lib/generators/test_data/environment_file_generator.rb
@@ -5,10 +5,18 @@ module TestData
   class EnvironmentFileGenerator < Rails::Generators::Base
     def call
       create_file "config/environments/test_data.rb", <<~RUBY
+        # Load the development environment as a starting point
         require_relative "development"
 
         Rails.application.configure do
+          # Don't persist schema.rb or structure.sql after test_data is migrated
           config.active_record.dump_schema_after_migration = false
+
+          # Output `rails server` logs to standard output (Rails' server command
+          # currently hard-codes this to only "development")
+          #
+          # If you want output appended to log/test_data.log, remove this line.
+          config.logger = Logger.new($stdout)
         end
       RUBY
     end
