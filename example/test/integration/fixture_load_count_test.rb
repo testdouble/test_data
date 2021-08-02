6b9bee24367bad3ec3da06efc089767139e1734f
Justin Searls <searls@gmail.com>
2021-08-02 15:21:05 -0400

    Identified a significant performance issue in how rails caches fixtures
    seems super redundant (loading a fixture set for every class instead of just doling out the subset of cache for each one. fine for rails if loading into database exactly one time, but we're doing this N times b/c we're loading and reloading once per class

---
+++ example/test/integration/fixture_load_count_test.rb
@@ -0,0 +1,79 @@
+# Regression test to make sure we don't load fixtures too many times
+
+class HookCounter
+  def self.count
+    @call_count || 0
+  end
+
+  def self.count!
+    @call_count ||= 0
+    @call_count += 1
+  end
+end
+at_exit do
+  if HookCounter.count > 2 # could be 1 if :all runs first, 2 if :boops only does
+    raise "Rails Fixture load was called #{HookCounter.count} times, shouldn't be more than 2!"
+  end
+end
+
+require "test_helper"
+
+TestData.prevent_rails_fixtures_from_loading_automatically!
+
+TestData.config do |config|
+  config.after_rails_fixture_load {
+    HookCounter.count!
+  }
+end
+
+class PartialFixtureTest < ActiveSupport::TestCase
+  fixtures :boops
+
+  setup do
+    TestData.uses_rails_fixtures(self)
+  end
+
+  def test_has_only_boops
+    assert boops(:boop_1)
+    assert_raises(NameError) { method(:pants) }
+  end
+end
+
+class AllFixtureTest < ActiveSupport::TestCase
+  fixtures :all
+
+  setup do
+    TestData.uses_rails_fixtures(self)
+  end
+
+  def test_has_both
+    assert boops(:boop_1)
+    assert pants(:pant_1)
+  end
+end
+
+class AllFixtureTest2 < ActiveSupport::TestCase
+  fixtures :all
+
+  setup do
+    TestData.uses_rails_fixtures(self)
+  end
+
+  def test_has_both
+    assert boops(:boop_1)
+    assert pants(:pant_1)
+  end
+end
+
+class AllFixtureTest3 < ActiveSupport::TestCase
+  fixtures :all
+
+  setup do
+    TestData.uses_rails_fixtures(self)
+  end
+
+  def test_has_both
+    assert boops(:boop_1)
+    assert pants(:pant_1)
+  end
+end
