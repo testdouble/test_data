8226668b9cc838643eeb589e0b61d49712fa8728
Justin Searls <searls@gmail.com>
2021-09-13 16:27:02 -0400

    make it pass maybe

--- lib/test_data/detects_database_existence.rb
+++ lib/test_data/detects_database_existence.rb
@@ -1,8 +1,17 @@
 module TestData
   class DetectsDatabaseExistence
+    def initialize
+      @config = TestData.config
+    end
+
     def call
-      ActiveRecord::Base.connection.execute("select 1")
-      true
+      rows = ActiveRecord::Base.connection.execute <<~SQL
+        select datname database_name
+        from pg_catalog.pg_database
+      SQL
+      rows.any? { |row|
+        row["database_name"] == @config.database_name
+      }
     rescue ActiveRecord::NoDatabaseError
       false
     end
